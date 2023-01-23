a1440bcf5dbd8a6b40e7c78965e42a586541aa54
Chaseao <chaseao.oberg@gmail.com>
2023-01-23 13:21:09 -0500

    Dialogue System Started

--- Assets/Scripts/Dialogue System/TextParserTool.cs
+++ Assets/Scripts/Dialogue System/TextParserTool.cs
@@ -6,3 +6,29 @@ public class TextParserTool
 {
     
 }
+
+public class DialogueData
+{
+    public bool WickIsSpeaker;
+    public string Dialogue;
+}
+
+public class ConversationData
+{
+    public string Conversant;
+    public List<DialogueData> Dialogues;
+}
+
+public class DialogueSystemValidData
+{
+    public readonly static List<string> VALID_CONVERSANTS = new List<string>()
+    {
+        "Martyn",
+        "Belamont",
+        "Letter",
+        "Newspaper",
+        "Bottles",
+        "Brochures",
+        "Glass"
+    };
+}
