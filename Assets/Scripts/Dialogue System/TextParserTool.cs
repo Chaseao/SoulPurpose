cdd327a2fddff779d1a91d951dd03ad1c48df8bc
Chase <chaseao.oberg@gmail.com>
2023-01-23 14:11:08 -0500

    More Updates to the dialogue system

--- Assets/Scripts/Dialogue System/TextParserTool.cs
+++ Assets/Scripts/Dialogue System/TextParserTool.cs
@@ -7,16 +7,46 @@ public class TextParserTool
     
 }
 
+public class DialogueManager : SingletonMonoBehavior<DialogueManager>
+{
+    [SerializeField] List<DialogueSceneData> dialogueSceneData;
+
+    public void StartDialogue(DialogueSystemValidData.DIALOGUE_ID id)
+    {
+
+    }
+}
+
+[CreateAssetMenu(fileName = "New Data", menuName = "Dialogue/Data")]
+public class DialogueSceneData : ScriptableObject
+{
+    [SerializeField] ConversationData conversationData;
+}
+
+[System.Serializable]
 public class DialogueData
 {
     public bool WickIsSpeaker;
     public string Dialogue;
 }
 
+[System.Serializable]
 public class ConversationData
 {
+    public string ID;
     public string Conversant;
+    public string Gives;
+    public string Unlocks;
     public List<DialogueData> Dialogues;
+    public List<DialogueBranchData> Choices;
+    public List<DialogueBranchData> LeadsTo;
+}
+
+[System.Serializable]
+public class DialogueBranchData
+{
+    public string BranchText;
+    public List<string> Requirments;
 }
 
 public class DialogueSystemValidData
@@ -30,5 +60,15 @@ public class DialogueSystemValidData
         "Bottles",
         "Brochures",
         "Glass"
+    };
+
+    public enum DIALOGUE_ID
+    {
+        Glass1,
+        Glass2,
+        Glass3,
+        Letter1,
+        Letter2,
+        Letter3
     };
 }
