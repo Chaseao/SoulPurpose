a1745b0da5e73689e3c972bf10613f671141a44c
Chase <chaseao.oberg@gmail.com>
2023-01-25 14:12:48 -0500

    Text UI Updates

---
+++ Assets/Scripts/Dialogue System/ImageDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/ImageDatabase")]
+public class ImageDatabase : ScriptableObject
+{
+    [SerializeField] Dictionary<string, Sprite> portraits;
+
+    public Sprite GetPortrait(string characterName)
+    {
+        return portraits[characterName];
+    }
+}
---
+++ Assets/Scripts/Dialogue System/TextBoxDisplay.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI dialogueTextField;
+    [SerializeField] Image textBoxImage;
+
+    public void Display() => ToggleChildrenDisplay(true);
+
+    public void UpdateDialogueText(string text, bool wickIsSpeaking)
+    {
+        dialogueTextField.text = text;
+        textBoxImage.rectTransform.rotation = new Quaternion() { y = wickIsSpeaking ? 180 : 0 };
+    }
+
+    public void Hide() => ToggleChildrenDisplay(false);
+
+    private void ToggleChildrenDisplay(bool shouldDisplay)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(shouldDisplay);
+        }
+    }
+}
