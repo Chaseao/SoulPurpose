bfb44fc6a4334abfded082aa8130f902befb92d1
Chaseao <chaseao.oberg@gmail.com>
2023-02-04 16:56:49 -0500

    Added fading in and out for audio

--- Assets/AudioControls.cs
+++ Assets/AudioControls.cs
@@ -6,14 +6,31 @@ using Sirenix.OdinInspector;
 public class AudioControls : SerializedMonoBehaviour
 {
     [SerializeField] Dictionary<int, AudioSource> audioFiles = new Dictionary<int, AudioSource>();
+    [SerializeField] float adjustmentSpeed;
 
+    int[] desiredVolumes = { 50, 0, 0, 0, 0, 0};
     public void SetAudio(int[] audioVolumes)
     {
-        for (int i = 0; i < audioVolumes.Length; i++)
+        desiredVolumes = audioVolumes;
+    }
+
+    private void Update()
+    {
+        MoveTowardsDesiredVolumes();
+    }
+
+    private void MoveTowardsDesiredVolumes()
+    {
+        for (int i = 0; i < desiredVolumes.Length; i++)
         {
-            if (audioFiles.ContainsKey(i))
+            if (audioFiles.ContainsKey(i) && audioFiles[i].volume != desiredVolumes[i])
             {
-                audioFiles[i].volume = audioVolumes[i] / 100f;
+                float directionToIncrement = Mathf.Sign(desiredVolumes[i] / 100f - audioFiles[i].volume);
+                float amountToIncrment = adjustmentSpeed * Time.deltaTime / 100f;
+                audioFiles[i].volume += directionToIncrement * amountToIncrment;
+                audioFiles[i].volume = directionToIncrement > 0
+                    ? Mathf.Min(audioFiles[i].volume, desiredVolumes[i] / 100f)
+                    : Mathf.Max(audioFiles[i].volume, desiredVolumes[i] / 100f);
             }
         }
     }
