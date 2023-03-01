1b7b14ae044d71ef1978cb5af264340e9a167d7f
Alex Contreras <alexsmart007@hotmail.com>
2023-03-01 09:26:20 -0500

    Added in the invisible collider (wall) for Hub world

--- Assets/Scripts/Interact/Items/Portal.cs
+++ Assets/Scripts/Interact/Items/Portal.cs
@@ -6,10 +6,8 @@ public class Portal : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        print("Should get here");
         if (other.gameObject.transform.CompareTag("Player"))
         {
-            print("Yo");
             if (SceneTools.NextSceneExists)
             {
                 StartCoroutine(SceneTools.TransitionToScene(SceneTools.NextSceneIndex));
